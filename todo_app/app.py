19ec1e38a2d9ff5a17b865486f2de2298e8ef238
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-11-22 11:02:55 +0000

    get and add works on site

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
-
+from flask import Flask, redirect,url_for,request
+from flask import render_template
 from todo_app.flask_config import Config
+from todo_app.data.session_items import get_items,add_item
 
 app = Flask(__name__)
 app.config.from_object(Config())
@@ -8,4 +9,10 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    list_items = get_items()
+    return render_template('index.html',Items = list_items)
+@app.route('/add',methods = ['POST'])
+def add():
+    title = request.form['toadd']
+    add_item(title)
+    return redirect(url_for('index'))
