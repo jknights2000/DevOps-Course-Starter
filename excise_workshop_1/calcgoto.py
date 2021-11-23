70e470b0b67ccd1c96d5efd8a954e5c430a927d0
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-11-23 11:53:54 +0000

    Update calcgoto.py

--- excise_workshop_1/calcgoto.py
+++ excise_workshop_1/calcgoto.py
@@ -4,6 +4,8 @@ from calc import add,subtract,divide,multi
 
 f = open("step_3.txt", "r")
 file = f.readlines()
+numbers = []
+calcs = []
 currentline = 0
 while currentline <= len(file) - 1:
     line = file[int(currentline)]
@@ -11,10 +13,18 @@ while currentline <= len(file) - 1:
     split = output.split(" ")
     print(split)
     if len(split) == 2:
-        currentline = int(split[1]) - 1
+        if int(split[1]) in numbers:
+            break
+        else:
+            currentline = int(split[1]) - 1
+            numbers.append(int(split[1]))
     else:
+        newdict = {"op":split[2],"n1":split[3],"n2":split[3]}
+        if newdict in calcs:
+                break
+            
         if split[2] == "+":
-           currentline = add(int(split[3]),int(split[4])) - 1
+            currentline = add(int(split[3]),int(split[4])) - 1
         elif split[2] == "-":
             currentline = subtract(int(split[3]),int(split[4])) - 1
         elif split[2] == "/":
