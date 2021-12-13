420fff56aad8eae4f433e401d5a16f6c644230ef
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-12-13 10:58:29 +0000

    added comments

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -6,9 +6,10 @@ def get_items():
     boardid = os.getenv('TRELLO_BOARDID')
     token = os.getenv('TRELLO_TOKEN')
     key = os.getenv('TRELLO_KEY')
+    #get nessary items from env
     items = []
     res1 = requests.get('https://api.trello.com/1/boards/'+boardid+'/cards?key='+ key +'&token='+token)
-
+    ##gets cards on board
    
     reponse1 = res1.json()
 
@@ -16,15 +17,20 @@ def get_items():
         listid = item['idList']
 
         res2 = requests.get('https://api.trello.com/1/lists/'+listid+'?key='+ key +'&token='+token)
+        ##use list id to get name of list
         reponse2 = res2.json()
 
       
         items.append(Item.from_trello(item,reponse2))
+        #makes an item object based on two reposnses
+    
     return session.get('items',items)
+    #sends to session
 
 def get_item(id):
     items = get_items()
     return next((item for item in items if item.id == int(id)), None)
+    #gets all items then compairs to one with same id
 
 def add_item(title,desc,date):
     
@@ -34,15 +40,11 @@ def add_item(title,desc,date):
     
     url = f"https://api.trello.com/1/cards"
     
-    if date is None:
-        querystring = {"name": title,"desc":desc, "idList": TODO, "key": key, "token": token}
-    else:
-        querystring = {"name": title,"desc":desc,"due":date, "idList": TODO, "key": key, "token": token}
+    querystring = {"name": title,"desc":desc,"due":date, "idList": TODO, "key": key, "token": token}#make query based on inputs
 
 
-    response = requests.request("POST", url, params=querystring)
-    card_id = response.json()["id"]
-    return card_id
+    requests.request("POST", url, params=querystring) # make post request using querystring as a parameter to the url
+   
 
 def save_item(item):
     token = os.getenv('TRELLO_TOKEN')
@@ -50,7 +52,7 @@ def save_item(item):
 
     url = f"https://api.trello.com/1/cards"
     querystring = {"cardid":item['cardid'], "key": key, "token": token}
-    requests.request("PUT", url, params=querystring)
+    requests.request("PUT", url, params=querystring)#saves card
 
 def complete(id):
     token = os.getenv('TRELLO_TOKEN')
@@ -59,7 +61,7 @@ def complete(id):
 
     updateitem = get_item(id)
     
-    reqUrl = f"https://api.trello.com/1/cards/{updateitem.cardid}?idList={done}&key={key}&token={token}"
+    reqUrl = f"https://api.trello.com/1/cards/{updateitem.cardid}?idList={done}&key={key}&token={token}"#changes list loaction to the done list
 
     headersList = {
     "Accept": "*/*",
@@ -71,10 +73,10 @@ def complete(id):
     requests.request("PUT", reqUrl, data=payload,  headers=headersList)
 
 def status(item):
-    return item.status
+    return item.status # used for filter
 
 def uncompleted(item):
-    if item.status == "TODO":
+    if item.status == "TODO":#method that returns true or false based on status. used for sorting
         return True
     return False
 
@@ -83,8 +85,9 @@ def todeleteitem(id):
     key = os.getenv('TRELLO_KEY')
     updateitem = get_item(id)
     id = updateitem.cardid
+    #gets the card to be deleted id
     reqUrl = f"https://api.trello.com/1/cards/{id}?key={key}&token={token}"
-
+    #make url
     headersList = {
     "Accept": "*/*",
         "User-Agent": "Thunder Client (https://www.thunderclient.io)" 
@@ -92,7 +95,7 @@ def todeleteitem(id):
 
     payload = ""
 
-    requests.request("DELETE", reqUrl, data=payload,  headers=headersList)
+    requests.request("DELETE", reqUrl, data=payload,  headers=headersList)#does a delete request
 
 class Item:
     def __init__(self, id, name, card_id, listid,desc,due, status = 'To Do'):
@@ -104,10 +107,10 @@ class Item:
         self.desc = desc
         
         if due is None:
-            self.due = due
-        else:
+            self.due = due#if none pass it as none
+        else:#otherwise format the date
             duedate = datetime.datetime.strptime(due,'%Y-%m-%dT%H:%M:%S.%fz')
             self.due = duedate.strftime('due %b %d, %Y')
     @classmethod
-    def from_trello(cls,card,list):
+    def from_trello(cls,card,list):#converts trello reponse into an item object
         return cls(card['idShort'],card['name'],card['id'],card['idList'],card['desc'],card['due'],list['name'])
