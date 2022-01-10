1a73b628eb219737d35deea0b796b219471acbcd
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2022-01-10 10:03:35 +0000

    unit done integration started

--- todo_app/app.py
+++ todo_app/app.py
@@ -3,36 +3,36 @@ from flask import render_template
 from todo_app.flask_config import Config
 from todo_app.data.trello_items import get_items,add_item,complete,status,uncompleted,todeleteitem
 from todo_app.data.ViewModel import ViewModel
-
-app = Flask(__name__)
-app.config.from_object(Config())
-
-
-@app.route('/')
-def index():
-    list_items = sorted(get_items(),key = status)
-    list_completed = get_items()
-    deletelist = get_items()
-    new_list = list(filter(uncompleted,list_completed))
-    view_model = ViewModel(list_items,new_list,deletelist)
-    return render_template('index.html',view_model = view_model)
-@app.route('/add',methods = ['POST'])
-def add():
-    title = request.form.get('toaddname')
-    desc = request.form.get('toadddesc',' ')
-    date = request.form.get('toadddate',None)
-    add_item(title,desc,date)
-    return redirect(url_for('index'))
+def create_app():
+    app = Flask(__name__)
     
-@app.route('/complete',methods = ['POST'])
-def done():
-    id = request.form['tocomplete']
-    complete(id)
-    return redirect(url_for('index'))
 
-@app.route('/delete',methods = ['POST'])
-def todelete():
-    newid = request.form['todelete']
-    todeleteitem(newid)
-    return redirect(url_for('index'))
 
+    @app.route('/')
+    def index():
+        list_items = sorted(get_items(),key = status)
+        list_completed = get_items()
+        deletelist = get_items()
+        new_list = list(filter(uncompleted,list_completed))
+        view_model = ViewModel(list_items,new_list,deletelist)
+        return render_template('index.html',view_model = view_model)
+    @app.route('/add',methods = ['POST'])
+    def add():
+        title = request.form.get('toaddname')
+        desc = request.form.get('toadddesc',' ')
+        date = request.form.get('toadddate',None)
+        add_item(title,desc,date)
+        return redirect(url_for('index'))
+    
+    @app.route('/complete',methods = ['POST'])
+    def done():
+        id = request.form['tocomplete']
+        complete(id)
+        return redirect(url_for('index'))
+
+    @app.route('/delete',methods = ['POST'])
+    def todelete():
+        newid = request.form['todelete']
+        todeleteitem(newid)
+        return redirect(url_for('index'))
+    return app
--- todo_app/test_app.py
+++ todo_app/test_app.py
@@ -18,15 +18,3 @@ def test_Done():
     test_model = viewmodel_test()
     test_todolist = test_model.getDone()
     assert len(test_todolist) == 1
-def test_GetItems(monkeypatch):
-    test_model = viewmodel_test()
-    test_list = get_items()
-    monkeypatch.setattr(get_items, 'get', stub)
-    assert test_list[0] == test_model.items[0]
-def stub():
-    return StubResponse
-class StubResponse(): 
- def Itemlist(self): 
-    return {Item("1", "bob", "card_id", "listid","desc","2000-12-02T12:50:30.07z", status = 'TODO')
-    ,Item("1", "bob", "card_id", "listid","desc","2000-12-02T12:50:30.07z", status = 'Doing')
-    ,Item("1", "bob", "card_id", "listid","desc","2000-12-02T12:50:30.07z", status = 'Done')}
