e37a03d82dabf8009cf3254c402a3e9f46acd230
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2022-08-08 14:33:15 +0100

    added most changes

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -2,75 +2,51 @@ import requests
 import os
 import datetime
 from flask import session
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 def get_items():
-    boardid = os.getenv('TRELLO_BOARDID')
-    token = os.getenv('TRELLO_TOKEN')
-    key = os.getenv('TRELLO_KEY')
+    client = MongoClient(os.getenv('CONNECTION_STRING'))
+    db = client.project
+    collection = db.Items
     #get nessary items from env
-    items = []
-    res1 = requests.get('https://api.trello.com/1/boards/'+boardid+'/cards?key='+ key +'&token='+token)
-    ##gets cards on board
-   
-    reponse1 = res1.json()
-
-    for item in reponse1:
-        listid = item['idList']
-
-        res2 = requests.get('https://api.trello.com/1/lists/'+listid+'?key='+ key +'&token='+token)
-        ##use list id to get name of list
-        reponse2 = res2.json()
-
-      
-        items.append(Item.from_trello(item,reponse2))
-        #makes an item object based on two reposnses
-    
-    return session.get('items',items)
+    return session.get('items',collection.find())
     #sends to session
 
 def get_item(id):
-    items = get_items()
-    return next((item for item in items if item.id == int(id)), None)
+    client = MongoClient(os.getenv('CONNECTION_STRING'))
+    document = client.db.collection.find_one({'_id': ObjectId(id)})
+    return document  
     #gets all items then compairs to one with same id
 
 def add_item(title,desc,date):
     
-    token = os.getenv('TRELLO_TOKEN')
-    key = os.getenv('TRELLO_KEY')
-    TODO = os.getenv('TRELLO_TODOID')
-    
-    url = f"https://api.trello.com/1/cards"
-    
-    querystring = {"name": title,"desc":desc,"due":date, "idList": TODO, "key": key, "token": token}#make query based on inputs
-
-
-    requests.request("POST", url, params=querystring) # make post request using querystring as a parameter to the url
+    client = MongoClient(os.getenv('CONNECTION_STRING'))
+    db = client.project
+    collection = db.Items
+    item = {"name": title,
+        "desc": desc,
+        "due": date,
+        "satus": "TODO"}
+    collection.insert_one(item)
    
 
 def save_item(item):
-    token = os.getenv('TRELLO_TOKEN')
-    key = os.getenv('TRELLO_KEY')
-
-    url = f"https://api.trello.com/1/cards"
-    querystring = {"cardid":item['cardid'], "key": key, "token": token}
-    requests.request("PUT", url, params=querystring)#saves card
+    client = MongoClient(os.getenv('CONNECTION_STRING'))
+    db = client.project
+    collection = db.Items
+    newvalues = { "$set": {"name": item.title,
+        "desc": item.desc,
+        "due": item.date,
+        "satus": item.status} }
+    collection.update_one({'_id': ObjectId(item.id)}, newvalues)
 
 def complete(id):
-    token = os.getenv('TRELLO_TOKEN')
-    key = os.getenv('TRELLO_KEY')
-    done = os.getenv('TRELLO_DONEID')
-
-    updateitem = get_item(id)
-    
-    reqUrl = f"https://api.trello.com/1/cards/{updateitem.cardid}?idList={done}&key={key}&token={token}"#changes list loaction to the done list
-
-    headersList = {
-    "Accept": "*/*",
-    "User-Agent": "Thunder Client (https://www.thunderclient.io)" 
-    }
-
-    payload = ""
-
-    requests.request("PUT", reqUrl, data=payload,  headers=headersList)
+    client = MongoClient(os.getenv('CONNECTION_STRING'))
+    db = client.project
+    collection = db.Items
+    newvalues = { "$set": {
+        "satus": "Done"} }
+    collection.update_one({'_id': ObjectId(id)}, newvalues)
 
 def status(item):
     return item.status # used for filter
@@ -81,36 +57,19 @@ def uncompleted(item):
     return False
 
 def todeleteitem(id):
-    token = os.getenv('TRELLO_TOKEN')
-    key = os.getenv('TRELLO_KEY')
-    updateitem = get_item(id)
-    id = updateitem.cardid
-    #gets the card to be deleted id
-    reqUrl = f"https://api.trello.com/1/cards/{id}?key={key}&token={token}"
-    #make url
-    headersList = {
-    "Accept": "*/*",
-        "User-Agent": "Thunder Client (https://www.thunderclient.io)" 
-    }
-
-    payload = ""
-
-    requests.request("DELETE", reqUrl, data=payload,  headers=headersList)#does a delete request
+    client = MongoClient(os.getenv('CONNECTION_STRING'))
+    db = client.project
+    collection = db.Items
+    collection.delete_one({'_id': ObjectId(id)})
 
 class Item:
-    def __init__(self, id, name, card_id, listid,desc,due, status = 'To Do'):
+    def __init__(self, id, name,desc,due, status = 'To Do'):
         self.id = id
         self.name = name
         self.status = status 
-        self.cardid = card_id
-        self.listid = listid
         self.desc = desc
-        
         if due is None:
             self.due = due#if none pass it as none
         else:#otherwise format the date
             duedate = datetime.datetime.strptime(due,'%Y-%m-%dT%H:%M:%S.%fz')
             self.due = duedate.strftime('due %b %d, %Y')
-    @classmethod
-    def from_trello(cls,card,list):#converts trello reponse into an item object
-        return cls(card['idShort'],card['name'],card['id'],card['idList'],card['desc'],card['due'],list['name'])
--- todo_app/test_unit.py
+++ todo_app/test_unit.py
@@ -2,9 +2,9 @@ import pytest
 from todo_app.data.trello_items import get_items,add_item,complete,status,uncompleted,todeleteitem,Item
 from todo_app.data.ViewModel import ViewModel
 def viewmodel_test():
-    list_new = [Item("1", "name1", "card_id1", "listid1","desc","2000-12-02T12:50:30.07z", status = 'TODO')
-    ,Item("2", "name2", "card_id2", "listid2","desc",None, status = 'Doing')
-    ,Item("3", "name3", "card_id3", "listid3","desc",None, status = 'Done')]
+    list_new = [Item("1", "name1","desc","2000-12-02T12:50:30.07z", status = 'TODO')
+    ,Item("2", "name2","desc",None, status = 'Doing')
+    ,Item("3", "name3","desc",None, status = 'Done')]
     return ViewModel(list_new,list_new,list_new)
 def test_Todo():
     test_model = viewmodel_test()
